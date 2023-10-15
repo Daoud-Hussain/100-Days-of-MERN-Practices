abe23541c774b54107a47ec798340fe7e1686d26
Daoud-Hussain <daoudhussain302@gmail.com>
2023-10-16 00:19:07 +0500

    Storing data in mongo

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const products = require("./Routes/products")
+const products = require("./Routes/products");
+const connectDB = require("./db/connect");
 
 app.get("/", (req, res) => {
-  res.send("Hi I am live");
+  res.send("I am live");
 });
 
 app.use("/home", products);
 
 try {
   app.listen(port, () => {
-    console.log(`${port} is live`);
+    console.log(`Site is live on ${port}`);
   });
 } catch (error) {
   console.log(error);
---
+++ db/connect.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const connectDB = () => {
+  mongoose
+    .connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("MongoDB connected");
+    })
+    .catch((err) => {
+      console.error("MongoDB connection error: ", err);
+    });
+};
+
+module.exports = connectDB;
