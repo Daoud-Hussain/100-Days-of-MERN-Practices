77d91570315edbf41c6f5e60bde30c63be77f15b
Daoud-Hussain <daoudhussain302@gmail.com>
2023-12-14 22:28:22 +0500

    Displaying only selected sorted data

--- Controllers/student.js
+++ Controllers/student.js
@@ -6,16 +6,17 @@ const getAllStudents  = async (req, res)=>{
     // let data = await Student.find({});
 
     //Displaying all the student records after sorting with names
-    // let data = await Student.find({}).sort("name");
+    let allData = await Student.find({}).sort("name");
+    // res.send({allData});
 
     //Displaying all the student records after sorting with name and selecting name and rollno to display
-    let data = await Student.find({}).sort("name").select("name rollno");
-    res.send({data});
+    let selectedData = await Student.find({}).sort("name").select("name rollno");
+    res.send({selectedData});
     // await Student.deleteMany({});
 }
 
 const getTestStudents = async (req, res) =>{
-    let specificData = await Student.find({name: "Daoud Hussain"});
+    let specificData = await Student.find({name: "Daoud Hussain", rollno: "SP21-BCS-102"});
     res.send({specificData});}
 
 module.exports = {getTestStudents, getAllStudents};
